bb11163b833a38acb56d2af5307b9cc012b3efa9
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-02 18:17:42 +0530

    Time: 3 ms (88.15%) | Memory: 43.1 MB (84.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,25 +2,17 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack=new ArrayDeque<>();
-        Deque<Character> type=new ArrayDeque<>();
-        type.push(' ');
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='('||c=='{'||c=='['){
-                stack.push(s.charAt(i));
-                if(c=='('){
-                    type.push(')');
-                }else if(c=='{'){
-                    type.push('}');
-                }else{
-                    type.push(']');
-                }
-            }else{
-                if(!stack.isEmpty()&&(type.peek()==s.charAt(i))){
-                    stack.pop();
-                    type.pop();
-                }else if(!(type.peek()==s.charAt(i))){
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
                     return false;
                 }
             }
