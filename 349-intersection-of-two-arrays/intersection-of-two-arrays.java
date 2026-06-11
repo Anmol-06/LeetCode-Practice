144600943a108a59e151a94f533e93ff3340649c
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-11 16:23:14 +0530

    Time: 2 ms (98.02%) | Memory: 45.1 MB (25.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,20 +1,50 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         ArrayList<Integer> intersection=new ArrayList<>();
+//         HashMap<Integer,Boolean> hashTable=new HashMap<>();
+//         for(int i : nums1){
+//             hashTable.put(i,true);
+//         }
+//         for(int j : nums2){
+//             if(hashTable.containsKey(j)){
+//                 intersection.add(j);
+//                 hashTable.remove(j);
+//             }
+//         }
+//         int[] resIntersection=intersection.stream().mapToInt(Integer::intValue).toArray();
+//         return resIntersection;
+//     }
+// }
+
+import java.util.HashSet;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> intersection=new ArrayList<>();
-        HashMap<Integer,Boolean> hashTable=new HashMap<>();
-        for(int i : nums1){
-            hashTable.put(i,true);
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersect = new HashSet<>();
+        
+        // Add all of nums1 to a Set
+        for (int i : nums1) {
+            set.add(i);
         }
-        for(int j : nums2){
-            if(hashTable.containsKey(j)){
-                intersection.add(j);
-                hashTable.remove(j);
+        
+        // If the number is in our first set, add it to our intersect set
+        for (int j : nums2) {
+            if (set.contains(j)) {
+                intersect.add(j);
             }
         }
-        int[] resIntersection=intersection.stream().mapToInt(Integer::intValue).toArray();
-        return resIntersection;
+        
+        // Manually convert to array (Much faster than Streams!)
+        int[] result = new int[intersect.size()];
+        int index = 0;
+        for (int num : intersect) {
+            result[index++] = num;
+        }
+        
+        return result;
     }
 }
