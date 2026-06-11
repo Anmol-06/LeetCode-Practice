b8e7ef722df2b2c7bf3f5d1daf66f1f987e6f39e
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-11 16:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (95.89%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+
+//Method-1
 // class Solution {
 //     public int[] intersection(int[] nums1, int[] nums2) {
 //         ArrayList<Integer> intersection=new ArrayList<>();
@@ -19,32 +21,52 @@ import java.util.List;
 //     }
 // }
 
-import java.util.HashSet;
+//Method-2
+// import java.util.HashSet;
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         HashSet<Integer> set = new HashSet<>();
+//         HashSet<Integer> intersect = new HashSet<>();
+//         for (int i : nums1) {
+//             set.add(i);
+//         }
+//         for (int j : nums2) {
+//             if (set.contains(j)) {
+//                 intersect.add(j);
+//             }
+//         }
+//         int[] result = new int[intersect.size()];
+//         int index = 0;
+//         for (int num : intersect) {
+//             result[index++] = num;
+//         }
+//         return result;
+//     }
+// }
+
+import java.util.Arrays;
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> intersect = new HashSet<>();
+        // Since max value is 1000, we make an array of size 1001
+        boolean[] seen = new boolean[1001];
         
-        // Add all of nums1 to a Set
-        for (int i : nums1) {
-            set.add(i);
+        for (int num : nums1) {
+            seen[num] = true; // Mark the number as seen
         }
         
-        // If the number is in our first set, add it to our intersect set
-        for (int j : nums2) {
-            if (set.contains(j)) {
-                intersect.add(j);
+        // Create an array to hold the maximum possible overlapping items
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        
+        for (int num : nums2) {
+            if (seen[num]) {
+                result[index++] = num;
+                seen[num] = false; // Set to false so we don't add it again
             }
         }
         
-        // Manually convert to array (Much faster than Streams!)
-        int[] result = new int[intersect.size()];
-        int index = 0;
-        for (int num : intersect) {
-            result[index++] = num;
-        }
-        
-        return result;
+        // Trim the empty spaces off the array before returning
+        return Arrays.copyOf(result, index);
     }
 }
