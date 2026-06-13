87e638ee0d078f7f6be52c2d9d85837c2145aba0
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-13 13:50:01 +0530

    Time: 2 ms (96.85%) | Memory: 46.3 MB (89.99%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.java
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public String mapWordWeights(String[] words, int[] weights) {
+//         String res="";
+        
+//         for(String str : words){
+//             int sum=0;
+//             for(char ch : str.toCharArray()){
+//                 int index=(int)ch-97;
+//                 sum+=weights[index];
+//             }
+//             int place=(25-sum%26)+97;
+//             char c=(char) place;
+//             res+=c;
+//         }
+//         return res;
+//     }
+// }
+
+//Alt-Method
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        String res="";
+        StringBuilder res = new StringBuilder(); 
         
-        for(String str : words){
-            int sum=0;
-            for(char ch : str.toCharArray()){
-                int index=(int)ch-97;
-                sum+=weights[index];
+        for (String str : words) {
+            int sum = 0;
+            for (char ch : str.toCharArray()) {
+                int index = (int) ch - 97;
+                sum += weights[index];
             }
-            int place=(25-sum%26)+97;
-            char c=(char) place;
-            res+=c;
+            int place = (25 - sum % 26) + 97;
+            res.append((char) place); 
         }
-        return res;
+        return res.toString();
     }
 }
