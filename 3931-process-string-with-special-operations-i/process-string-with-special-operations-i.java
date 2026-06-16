a7fcdf185eb5f6f840ee161309a3b5dd9daf801f
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-16 11:30:24 +0530

    Time: 4 ms (76.47%) | Memory: 55.5 MB (36.76%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,11 +1,12 @@
 class Solution {
     public String processStr(String s) {
-        StringBuffer sb=new StringBuffer("");
+        StringBuilder sb=new StringBuilder("");
         for(int i=0;i<s.length();i++){
-            switch(s.charAt(i)){
+            char c=s.charAt(i);
+            switch(c){
                 case '*':
-                    if(sb.toString().length()!=0)
-                        sb.deleteCharAt(sb.toString().length()-1);
+                    if(sb.length()>0)
+                        sb.deleteCharAt(sb.length()-1);
                     break;
                 case '#':
                     sb.append(sb.toString());
@@ -14,7 +15,7 @@ class Solution {
                     sb.reverse();
                     break;
                 default:
-                    sb.append(s.charAt(i));
+                    sb.append(c);
                     break;
             }
         }
