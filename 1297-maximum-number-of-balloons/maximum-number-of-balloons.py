dbded2176c350f91c5059b14260fe51c666592c1
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-22 21:50:28 +0530

    Time: 2 ms (68.66%) | Memory: 19.3 MB (38.08%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,17 +1,9 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        dist={'b':0, 'a':0, 'l':0, 'o':0, 'n':0}
+        dist = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
         for char in text:
-            if char == 'b':
-                dist[char]=dist.get(char)+1
-            if char == 'a':
-                dist[char]=dist.get(char)+1
-            if char == 'l':
-                dist[char]=dist.get(char)+1
-            if char == 'o':
-                dist[char]=dist.get(char)+1
-            if char == 'n':
-                dist[char]=dist.get(char)+1
-        dist['l']=dist.get('l')//2
-        dist['o']=dist.get('o')//2
-        return min(dist.get('b'),dist.get('a'),dist.get('l'),dist.get('o'),dist.get('n'))
+            if char in dist:
+                dist[char] += 1
+        dist['l'] //= 2
+        dist['o'] //= 2
+        return min(dist.values())
