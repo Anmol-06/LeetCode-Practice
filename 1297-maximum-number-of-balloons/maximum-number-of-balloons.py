a1f4743962d51af194a99da2aed5a2102c7c172c
Anmol Kumar Gorain <akganmol@gmail.com>
2026-06-22 21:52:18 +0530

    Time: 1 ms (73.88%) | Memory: 19.4 MB (9.97%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,9 +1,22 @@
+# class Solution:
+#     def maxNumberOfBalloons(self, text: str) -> int:
+#         dist = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+#         for char in text:
+#             if char in dist:
+#                 dist[char] += 1
+#         dist['l'] //= 2
+#         dist['o'] //= 2
+#         return min(dist.values())
+
+from collections import Counter
+
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        dist = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
-        for char in text:
-            if char in dist:
-                dist[char] += 1
-        dist['l'] //= 2
-        dist['o'] //= 2
-        return min(dist.values())
+        counts = Counter(text)
+        return min(
+            counts['b'], 
+            counts['a'], 
+            counts['l'] // 2, 
+            counts['o'] // 2, 
+            counts['n']
+        )
