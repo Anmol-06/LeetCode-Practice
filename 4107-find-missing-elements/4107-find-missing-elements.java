317256abbf33d46c311aa349feb42783318ab868
Anmol Kumar Gorain <akganmol@gmail.com>
2026-08-04 21:44:09 +0530

    4107-FIND-MISSING-ELEMENTS Stats: Time: 8 ms (12%), Space: 47.5 MB (5.33%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.java
+++ 4107-find-missing-elements/4107-find-missing-elements.java
@@ -4,7 +4,12 @@ class Solution {
         Arrays.sort(nums);
         List<Integer> lst=new ArrayList<>();
         for(int i=nums[0];i<=nums[nums.length-1];i++){
-            boolean found = Arrays.binarySearch(nums, i) >= 0;
+            boolean found = false;
+            for(int n:nums){
+                if(n==i){
+                    found=true;
+                }
+            }
             if(!found){
                 lst.add(i);
             }
